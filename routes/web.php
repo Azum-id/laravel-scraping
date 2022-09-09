5fe56f355dcfdf3a5afe80b44486c8b8e914630a
Azum-id <cayna666id@gmail.com>
2022-09-09 13:11:57 +0700

    Fix validation Instagram

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::get('/instagram', function () {
         } else {
             $data = $json->graphql->shortcode_media;
             $data = json_decode(json_encode($data), true);
-            $type = $data['__typename'];
+            $type = (isset($data['__typename']) && !empty($data['__typename'])) ? $data['__typename'] : 'unknown';
             switch ($type) {
                 case 'GraphImage':
                     $result = [
@@ -44,18 +44,19 @@ Route::get('/instagram', function () {
                         'code' => 200,
                         'type' => 'image',
                         'data' => [
-                            'image' => $data['display_resources'],
-                            'high_resolution' => collect($data['display_resources'])->last(),
-                            'caption' => $data['edge_media_to_caption']['edges'][0]['node']['text'],
-                            'shortcode' => $data['shortcode'],
-                            'taken_at_timestamp' => $data['taken_at_timestamp'],
+                            'image' => (isset($data['display_resources']) && !empty($data['display_resources'])) ? $data['display_resources'] : null,
+                            // 'high_resolution' => collect($data['display_resources'])->last(),
+                            'high_resolution' => (isset($data['display_resources']) && !empty($data['display_resources'])) ? collect($data['display_resources'])->last() : null,
+                            'caption' => (isset($data['edge_media_to_caption']['edges'][0]['node']['text']) && !empty($data['edge_media_to_caption']['edges'][0]['node']['text'])) ? $data['edge_media_to_caption']['edges'][0]['node']['text'] : null,
+                            'shortcode' => (isset($data['shortcode']) && !empty($data['shortcode'])) ? $data['shortcode'] : null,
+                            'taken_at_timestamp' => (isset($data['taken_at_timestamp']) && !empty($data['taken_at_timestamp'])) ? $data['taken_at_timestamp'] : null,
                             'owner' => [
-                                'id' => $data['owner']['id'],
-                                'username' => $data['owner']['username'],
-                                'full_name' => $data['owner']['full_name'],
-                                'profile_pic_url' => $data['owner']['profile_pic_url'],
-                                'is_private' => $data['owner']['is_private'],
-                                'is_verified' => $data['owner']['is_verified'],
+                                'id' => (isset($data['owner']['id']) && !empty($data['owner']['id']) ? $data['owner']['id'] : null),
+                                'username' => (isset($data['owner']['username']) && !empty($data['owner']['username']) ? $data['owner']['username'] : null),
+                                'full_name' => (isset($data['owner']['full_name']) && !empty($data['owner']['full_name']) ? $data['owner']['full_name'] : null),
+                                'profile_pic_url' => (isset($data['owner']['profile_pic_url']) && !empty($data['owner']['profile_pic_url']) ? $data['owner']['profile_pic_url'] : null),
+                                'is_private' => (isset($data['owner']['is_private']) && !empty($data['owner']['is_private']) ? $data['owner']['is_private'] : null),
+                                'is_verified' => (isset($data['owner']['is_verified']) && !empty($data['owner']['is_verified']) ? $data['owner']['is_verified'] : null),
                             ],
                         ]
                     ];
@@ -68,17 +69,17 @@ Route::get('/instagram', function () {
                         'type' => 'video',
                         'data' => [
                             'video' => $data['video_url'],
-                            'caption' => $data['edge_media_to_caption']['edges'][0]['node']['text'],
-                            'shortcode' => $data['shortcode'],
-                            'video_duration' => $data['video_duration'],
-                            'taken_at_timestamp' => $data['taken_at_timestamp'],
+                            'caption' => (isset($data['edge_media_to_caption']['edges'][0]['node']['text']) && !empty($data['edge_media_to_caption']['edges'][0]['node']['text']) ? $data['edge_media_to_caption']['edges'][0]['node']['text'] : null),
+                            'shortcode' => (isset($data['shortcode']) && !empty($data['shortcode']) ? $data['shortcode'] : null),
+                            'video_duration' => (isset($data['video_duration']) && !empty($data['video_duration']) ? $data['video_duration'] : null),
+                            'taken_at_timestamp' => (isset($data['taken_at_timestamp']) && !empty($data['taken_at_timestamp']) ? $data['taken_at_timestamp'] : null),
                             'owner' => [
-                                'id' => $data['owner']['id'],
-                                'username' => $data['owner']['username'],
-                                'full_name' => $data['owner']['full_name'],
-                                'profile_pic_url' => $data['owner']['profile_pic_url'],
-                                'is_private' => $data['owner']['is_private'],
-                                'is_verified' => $data['owner']['is_verified'],
+                                'id' => (isset($data['owner']['id']) && !empty($data['owner']['id']) ? $data['owner']['id'] : null),
+                                'username' => (isset($data['owner']['username']) && !empty($data['owner']['username']) ? $data['owner']['username'] : null),
+                                'full_name' => (isset($data['owner']['full_name']) && !empty($data['owner']['full_name']) ? $data['owner']['full_name'] : null),
+                                'profile_pic_url' => (isset($data['owner']['profile_pic_url']) && !empty($data['owner']['profile_pic_url']) ? $data['owner']['profile_pic_url'] : null),
+                                'is_private' => (isset($data['owner']['is_private']) && !empty($data['owner']['is_private']) ? $data['owner']['is_private'] : null),
+                                'is_verified' => (isset($data['owner']['is_verified']) && !empty($data['owner']['is_verified']) ? $data['owner']['is_verified'] : null),
                             ],
                         ]
                     ];
@@ -86,26 +87,28 @@ Route::get('/instagram', function () {
                     break;
                 case 'GraphSidecar':
                     $arr_node = [];
-                    foreach ($data['edge_sidecar_to_children']['edges'] as $key => $value) {
-                        $arr_node[] = $value['node'];
+                    if (isset($data['edge_sidecar_to_children']['edges']) && !empty($data['edge_sidecar_to_children']['edges'])) {
+                        foreach ($data['edge_sidecar_to_children']['edges'] as $key => $value) {
+                            $arr_node[] = $value['node'];
+                        }
                     }
                     $result = [
                         'status' => true,
                         'code' => 200,
                         'type' => 'sidecar',
                         'data' => [
-                            'sidecar' => $arr_node,
-                            'caption' => $data['edge_media_to_caption']['edges'][0]['node']['text'],
-                            'shortcode' => $data['shortcode'],
-                            'slide_count' => count($data['edge_sidecar_to_children']['edges']),
-                            'taken_at_timestamp' => $data['taken_at_timestamp'],
+                            'sidecar' => (isset($arr_node) && !empty($arr_node) ? $arr_node : null),
+                            'caption' => (isset($data['edge_media_to_caption']['edges'][0]['node']['text']) && !empty($data['edge_media_to_caption']['edges'][0]['node']['text']) ? $data['edge_media_to_caption']['edges'][0]['node']['text'] : null),
+                            'shortcode' => (isset($data['shortcode']) && !empty($data['shortcode']) ? $data['shortcode'] : null),
+                            'slide_count' => (isset($data['edge_sidecar_to_children']['edges']) && !empty($data['edge_sidecar_to_children']['edges']) ? count($data['edge_sidecar_to_children']['edges']) : null),
+                            'taken_at_timestamp' => (isset($data['taken_at_timestamp']) && !empty($data['taken_at_timestamp']) ? $data['taken_at_timestamp'] : null),
                             'owner' => [
-                                'id' => $data['owner']['id'],
-                                'username' => $data['owner']['username'],
-                                'full_name' => $data['owner']['full_name'],
-                                'profile_pic_url' => $data['owner']['profile_pic_url'],
-                                'is_private' => $data['owner']['is_private'],
-                                'is_verified' => $data['owner']['is_verified'],
+                                'id' => (isset($data['owner']['id']) && !empty($data['owner']['id']) ? $data['owner']['id'] : null),
+                                'username' => (isset($data['owner']['username']) && !empty($data['owner']['username']) ? $data['owner']['username'] : null),
+                                'full_name' => (isset($data['owner']['full_name']) && !empty($data['owner']['full_name']) ? $data['owner']['full_name'] : null),
+                                'profile_pic_url' => (isset($data['owner']['profile_pic_url']) && !empty($data['owner']['profile_pic_url']) ? $data['owner']['profile_pic_url'] : null),
+                                'is_private' => (isset($data['owner']['is_private']) && !empty($data['owner']['is_private']) ? $data['owner']['is_private'] : null),
+                                'is_verified' => (isset($data['owner']['is_verified']) && !empty($data['owner']['is_verified']) ? $data['owner']['is_verified'] : null),
                             ],
                         ]
                     ];
@@ -132,7 +135,7 @@ Route::get('/instagram', function () {
             return [
                 'error' => true,
                 'code' => $th->getCode(),
-                'message' => 'Something went wrong'
+                'message' => 'Something went wrong',
             ];
         }
     }
